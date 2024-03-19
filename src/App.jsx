d7714fcd4caf2b479333062342cf8fe1aadfa8d1
Brenobn <brenobragabn@gmail.com>
2024-03-19 13:24:09 -0300

    Importing post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Header } from "./components/Header";
+import { Post }from './Post';
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -7,7 +10,17 @@ export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello world</h1>
+      <Post 
+        author="Breno Leonardo" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quibusdam quo nesciunt nostrum commodi dolor consequuntur laboriosam omnis dignissimos, quos doloremque facere velit, possimus veniam molestiae nemo, ipsa dolorem provident." 
+      />
+      
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+
+        </main>
+      </div>
     </div>
   )
 }
