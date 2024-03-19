d7c89bb8cc172d0b135a45ba95fc6c28cabe9bbb
Brenobn <brenobragabn@gmail.com>
2024-03-19 10:19:22 -0300

    Importing the Header into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Header } from "./components/Header";
+
+import './global.css';
+
 export function App() {
 
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Header />
+      <h1>Hello world</h1>
+    </div>
   )
 }
